3b376ec6f73c05ce307cbb6e8278e7ca6486ec47
Matinalz <930075720@qq.com>
2021-08-14 16:11:06 +0800

    diyici

---
+++ src/Test.java
@@ -0,0 +1,11 @@
+/**
+ * @program:IntelliJ IDEA
+ * @Description:
+ * @Author:Matinal
+ * @Date:2021-08-14 15:55
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("fghjgj");
+    }
+}
